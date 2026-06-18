b1147735a69b357c650cc20574425369f8fb2323
fionayeshani <fionawijewardhana@gmail.com>
2026-06-18 10:23:54 +0900

    completed homework 2

--- lec02/homework2.py
+++ lec02/homework2.py
@@ -1,16 +1,26 @@
-'''
-This homework defines one method, called "arithmetic".
-that method, type `help homework2.arithmetic`.
-'''
-
-def arithmetic(x, y):
-    """
-    Modify this code so that it performs one of four possible functions, 
-    as specified in the following table:
-
-                        isinstance(x,str)  isinstance(x,float)
-    isinstance(y,str)   return x+y         return str(x)+y
-    isinstance(y,float) return x*int(y)    return x*y
-    """
-    return 0
-
+'''
+This homework defines one method, called "arithmetic".
+that method, type `help homework2.arithmetic`.
+'''
+
+def arithmetic(x, y):
+    """
+    Modify this code so that it performs one of four possible functions,
+    as specified in the following table:
+
+                        isinstance(x,str)  isinstance(x,float)
+    isinstance(y,str)   return x+y         return str(x)+y
+    isinstance(y,float) return x*int(y)    return x*y
+    """
+
+    if isinstance(x, str):
+        if isinstance(y, str):
+            return x + y
+        elif isinstance(y, float):
+            return x * int(y)
+
+    elif isinstance(x, float):
+        if isinstance(y, str):
+            return str(x) + y
+        elif isinstance(y, float):
+            return x * y
